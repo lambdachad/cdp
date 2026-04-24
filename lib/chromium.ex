06df4c523758cb00ce678d8bed8c6ec9c747d05a
Odd-Harald <oddharald@myhren.ai>
2026-04-24 22:34:32 +0300

    Great

--- lib/chromium.ex
+++ lib/chromium.ex
@@ -3,22 +3,28 @@ defmodule CDP.Chromium do
     # Spawn Chromium in headless mode on available port
     port = available_port()
     chromium_path = System.find_executable("chromium")
-    chromium = Port.open({:spawn_executable, chromium_path}, [
-      args: [
-        "--headless=new",
-        "--no-sandbox",
-        "--disable-gpu",
-        "--window-size=1920,1080",
-        "--remote-debugging-port=#{port}"
-      ]
-    ])
+    chromium =
+      Port.open({:spawn_executable, chromium_path},
+        args: [
+          "--headless=new",
+          "--no-sandbox",
+          "--disable-gpu",
+          "--window-size=1920,1080",
+          "--remote-debugging-port=#{port}"
+        ]
+      )
+    {:os_pid, os_pid} = Port.info(chromium, :os_pid)
 
     # Get WebSocket debugger URL
     version_url = "http://localhost:#{port}/json/version"
     {:ok, %{body: %{"webSocketDebuggerUrl" => ws_url}}} = Req.get(version_url)
-    %{ chromium: chromium, ws_url: ws_url }
+    %{chromium: chromium, os_pid: os_pid, ws_url: ws_url}
   end
-  
+
+  def kill(%{os_pid: os_pid}) do
+    System.cmd("kill", ["-9", "#{os_pid}"])
+  end
+
   defp available_port do
     # Create TCP socket then close it to get available port
     {:ok, port} = :gen_tcp.listen(0, [])
--- test/cdp_test.exs
+++ test/cdp_test.exs
@@ -1,8 +1,39 @@
-defmodule CDPTest do
+defmodule CDP.ChromiumTest do
   use ExUnit.Case
-  doctest CDP
 
-  test "greets the world" do
-    assert CDP.hello() == :world
+  test "spawn returns chromium port and ws_url" do
+    browser = CDP.Chromium.spawn()
+    on_exit(fn -> CDP.Chromium.kill(browser) end)
+
+    assert is_port(browser.chromium)
+    assert String.starts_with?(browser.ws_url, "ws://")
+  end
+end
+
+defmodule CDP.ClientTest do
+  use ExUnit.Case
+
+  setup do
+    browser = CDP.Chromium.spawn()
+    {:ok, client} = CDP.Client.start_link(browser.ws_url)
+    on_exit(fn -> CDP.Chromium.kill(browser) end)
+    %{browser: browser, client: client}
+  end
+
+  test "send_command returns browser version", %{client: client} do
+    {:ok, result} = CDP.Client.send_command(client, "Browser.getVersion")
+
+    assert result["product"] =~ "Chrome"
+    assert result["protocolVersion"]
+  end
+
+  test "send_command with params works", %{client: client} do
+    {:ok, result} =
+      CDP.Client.send_command(client, "Browser.setDownloadBehavior", %{
+        behavior: "allow",
+        downloadPath: "/tmp"
+      })
+
+    assert result == %{}
   end
 end
